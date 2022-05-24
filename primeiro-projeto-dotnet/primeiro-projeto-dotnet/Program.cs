5aa3d7217884b85b87a1939739f38a91b6b3ecd7
lisboamab <lisboamab@gmail.com>
2022-05-24 14:13:42 -0300

    first commit

---
+++ primeiro-projeto-dotnet/primeiro-projeto-dotnet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace primeiro_projeto_dotnet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double teste1 = 10.45;
+			Console.WriteLine(teste1.ToString("F2"));
+		}
+	}
+}
